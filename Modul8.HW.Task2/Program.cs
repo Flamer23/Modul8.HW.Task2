67d15a02e0a8d49410ceac7e870015ff0ce3db69
Илья Зуев <zueviliya777@gmail.com>
2024-01-12 04:13:54 +0300

    Все задания

--- Modul8.HW.Task2/Program.cs
+++ Modul8.HW.Task2/Program.cs
@@ -1,10 +1,80 @@
-﻿namespace Modul8.HW.Task2
+﻿using System.Security.Cryptography;
+
+namespace Modul8.HW.Task2
 {
+    
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C:\\Users\\zuevi\\Desktop\\HW";
+
+            long size = 0;
+
+            if (Directory.Exists(path))
+            {
+                Count(path, ref size);
+                Console.WriteLine($"{size} байт");
+            }
+            else
+            {
+                Console.WriteLine("Папка по указанному пути не существует");
+            }
+        }
+
+        public static void Count(string path, ref long size)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+
+            DirectoryInfo[] subDirectories = directory.GetDirectories();
+            FileInfo[] files = directory.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                try
+                {
+                    size += file.Length;
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine("Файл не найден. Ошибка: " + ex.Message);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine("Отсутствует доступ. Ошибка: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка: " + ex.Message);
+                }
+                                
+            }
+
+            foreach (DirectoryInfo subDirectory in subDirectories)
+            {
+                if(subDirectory.GetDirectories() != null)
+                {
+                    try
+                    {
+                        Count(subDirectory.FullName, ref size);
+                    }
+                    catch (DirectoryNotFoundException ex)
+                    {
+                        Console.WriteLine("Директория не найдена. Ошибка: " + ex.Message);
+                    }
+                    catch (UnauthorizedAccessException ex)
+                    {
+                        Console.WriteLine("Отсутствует доступ. Ошибка: " + ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Произошла ошибка: " + ex.Message);
+                    }
+                }
+                
+            }
+            
         }
     }
 }
